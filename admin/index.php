21e048c1d1559060aa048a1b70e716419c2c6303
ayeayemyint100314 <ayeayemyint0709@gmail.com>
2025-07-31 11:40:24 -0600

    Php server working now and tested one file and installation finished.

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$batch = 111; // integer datatype
+$name = "IMC"; // string data type
+$temp = 33.5; // float data type
+echo "<b><em>Hello Batch $batch, welcome to  ecommerce module</em></b>";
+
+?>
