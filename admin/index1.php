adae67f6cc2b99721a9916cf4fe5ec1dd22ba2d3
ayeayemyint100314 <ayeayemyint0709@gmail.com>
2025-08-01 09:45:31 -0600

    login page is partially done.

--- admin/index1.php
+++ admin/index1.php
@@ -10,7 +10,7 @@ function power($num1, $num2)
     return pow($num1, $num2);
 }
 
-$number1 = 2;
+$number1 = 2; // $studentName
 $number2 = 6;
 $result = power($number1, $number2);// calling function
 echo "<br>result is $result";
---
+++ admin/pwdTest.php
@@ -0,0 +1,6 @@
+<?php
+$hashCode = password_hash('Abc123!@#', PASSWORD_BCRYPT);
+echo $hashCode;
+
+
+
